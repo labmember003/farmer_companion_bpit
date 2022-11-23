4e75e70e999b1e4ca124ecaab053933b055276ff
Avishisht <avishishtgupta@gmail.com>
2022-11-23 15:41:28 +0530

    added tools chat activity

---
+++ app/src/main/java/com/labmember003/farmercompanion/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.labmember003.farmercompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.system.exitProcess
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
