bba96bd98b14073f3d657c9600633ab72100ec71
Avishisht <avishishtgupta@gmail.com>
2022-11-23 19:33:30 +0530

    completed lend activity

--- app/src/main/java/com/labmember003/farmercompanion/LendActivity.kt
+++ app/src/main/java/com/labmember003/farmercompanion/LendActivity.kt
@@ -2,10 +2,15 @@ package com.labmember003.farmercompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class LendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lend)
+        setContentView(R.layout.simple_android_contact_form_ui_design)
+        findViewById<Button>(R.id.submit).setOnClickListener {
+            Toast.makeText(this, "Successfully Submited", Toast.LENGTH_SHORT).show()
+        }
     }
 }
